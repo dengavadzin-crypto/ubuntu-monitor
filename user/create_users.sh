6cdf8b738054b0db3a403897f1695e5200358dc9
Denys <den.gavadzin@gmail.com>
2026-04-10 12:11:34 +0000

    Fix: create multiple users via arguments

--- user/create_users.sh
+++ user/create_users.sh
@@ -1,21 +1,14 @@
-#!/bin/bash
-
-USER_NAME="devops_user"
-
-# Проверяем, существует ли пользователь
-if id "$USER_NAME" &>/dev/null; then
-    echo "Внимание: Пользователь $USER_NAME уже существует в системе!"
-    echo "Никаких изменений не внесено."
-else
-    # Если пользователя нет, запускаем процесс создания
+for USER_NAME in "$@"; do
+  if id "$USER_NAME" &>/dev/null; then
+    echo "Внимание: Пользователь $USER_NAME уже существует!"
+  else
     USER_PASS=$(openssl rand -base64 12)
-    
     useradd -m -s /bin/bash "$USER_NAME"
     usermod -aG sudo "$USER_NAME"
     echo "$USER_NAME:$USER_PASS" | chpasswd
-    
     echo "------------------------------------------"
     echo "Пользователь $USER_NAME успешно создан!"
     echo "Пароль для входа: $USER_PASS"
     echo "------------------------------------------"
-fi
+  fi
+done
